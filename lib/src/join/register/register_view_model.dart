dde74f57a58c481e5e14f69f474b67a1661d059b
barisozen52 <32873234+barisozen52@users.noreply.github.com>
2022-03-14 18:29:50 +0300

    add ve delete servisleri eklendi

---
+++ lib/src/join/register/register_view_model.dart
@@ -0,0 +1,9 @@
+import 'package:davetcim/shared/services/database.dart';
+import 'package:flutter/cupertino.dart';
+
+class RegisterViewModel extends ChangeNotifier{
+  Database db = Database();
+
+
+
+}
