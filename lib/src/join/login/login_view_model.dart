11fbce884d517290fc4851ef08e1435e43115e6a
yusufenesdanalioglu <yusufenes3494@gmail.com>
2022-03-15 22:04:33 +0300

    fillusersession input parametresi customer model olarak degistirildi

--- lib/src/join/login/login_view_model.dart
+++ lib/src/join/login/login_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:davetcim/environments/db_constants.dart';
+import 'package:davetcim/shared/models/customer_model.dart';
 import 'package:davetcim/shared/services/database.dart';
 import 'package:davetcim/shared/sessions/application_session.dart';
 import 'package:davetcim/shared/sessions/user_session.dart';
@@ -9,42 +10,41 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewModel extends ChangeNotifier {
-
   Database db = Database();
 
   Future<void> userLoginFlow(BuildContext context, Widget childPage,
       String userName, String password) async {
-    var response = await db.getCollectionRef(DBConstants.customerDB)
+    var response = await db
+        .getCollectionRef(DBConstants.customerDB)
         .where('username', isEqualTo: userName)
-        .where('password', isEqualTo: password).get();
+        .where('password', isEqualTo: password)
+        .get();
 
     if (response.docs != null && response.docs.length > 0) {
       var list = response.docs;
-      fillUserSession(list[0].data());
+      Customer customer = Customer.fromMap(list[0].data());
+      fillUserSession(customer);
       Utils.navigateToPage(context, childPage);
     } else {
       Dialogs.showAlertMessage(
           context,
-          LanguageConstants.dialogUnSuccessHeader[
-          LanguageConstants.languageFlag],
-          LanguageConstants.kullaniciAdiYaDaParolaYanlis[
-          LanguageConstants.languageFlag]);
+          LanguageConstants
+              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
+          LanguageConstants
+              .kullaniciAdiYaDaParolaYanlis[LanguageConstants.languageFlag]);
     }
   }
 
-  void fillUserSession(Map item) {
+  void fillUserSession(Customer customer) {
     ApplicationSession.userSession = UserSession(
-        int.parse(item["id"].toString()),
-        item["name"].toString(),
-        item["surname"].toString(),
-        item["gsmNo"].toString(),
-        int.parse(item["corporationId"].toString()),
-        int.parse(item["roleId"].toString()),
-        item["isActive"],
-        item["username"].toString()
+      customer.id,
+      customer.name,
+      customer.surname,
+      customer.gsmNo,
+      customer.corporationId,
+      customer.roleId,
+      customer.isActive,
+      customer.username,
     );
   }
-
-
-
 }
