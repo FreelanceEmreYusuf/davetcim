c595b4ffdf86d2930282854e5f34ef2ca7ee08c6
barisozen52 <32873234+barisozen52@users.noreply.github.com>
2022-03-24 12:56:22 +0300

    getRegisterFormElementsControl extentionu eklendi ve form element kontrolleri adında extention oluşturuldu

--- lib/src/join/register/register_view_model.dart
+++ lib/src/join/register/register_view_model.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:davetcim/environments/const.dart';
 import 'package:davetcim/environments/db_constants.dart';
+import 'package:davetcim/shared/extentions/form_control_extention.dart';
 import 'package:davetcim/shared/models/customer_model.dart';
 import 'package:davetcim/shared/models/secret_questions_model.dart';
 import 'package:davetcim/shared/services/database.dart';
@@ -8,6 +11,7 @@ import 'package:davetcim/shared/utils/dialogs.dart';
 import 'package:davetcim/shared/utils/language.dart';
 import 'package:davetcim/shared/utils/utils.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../join_view.dart';
 
@@ -24,100 +28,12 @@ class RegisterViewModel extends ChangeNotifier {
       String email,
       String questionAnswer,
       SecretQuestionsModel selectedQuestion) async {
-
-    if (await formControls(context,username, password,name,surname,
+    FormControlExtention formControlExtention= FormControlExtention();
+    if (await formControlExtention.getRegisterFormElementsControl(context,username, password,name,surname,
       phoneNumber,email,questionAnswer,selectedQuestion)) {
-      await createCustomer(username, email, password, phoneNumber,
-          name, surname,selectedQuestion,questionAnswer);
+      await createCustomer(username, email, password, phoneNumber, name, surname,selectedQuestion,questionAnswer);
       showSucessMessage(context);
     }
-  }
-
-  Future<bool> formControls(
-      BuildContext context,
-      String userName,
-      String password,
-      String name,
-      String surname,
-      String phoneNumber,
-      String email,
-      String questionAnswer,
-      SecretQuestionsModel selectedQuestion) async {
-    CollectionReference docsRef = db.getCollectionRef(Constants.customerDB);
-    var response = await docsRef.where('userName', isEqualTo: userName).get();
-    if (response.docs != null && response.docs.length > 0) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.dialogRegisterUnSuccessUserNameMessage[
-          LanguageConstants.languageFlag]);
-      return false;
-    }
-    if (password.length < 8) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.dialogRegisterUnSuccessPasswordMessage[
-          LanguageConstants.languageFlag]);
-      return false;
-    }
-    if (name.length < 1) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.dialogRegisterUnSuccessFullNameMessage[
-          LanguageConstants.languageFlag]);
-      return false;
-    }
-    if (surname.length < 1) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.dialogRegisterUnSuccessFullNameMessage[
-          LanguageConstants.languageFlag]);
-      return false;
-    }
-    if (phoneNumber.length < 1) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.dialogRegisterUnSuccessPhoneNumberMessage[
-          LanguageConstants.languageFlag]);
-      return false;
-    }
-    if (!email.contains("@") || !email.contains(".") || email.length < 7) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.dialogRegisterUnSuccessEmailMessage[
-          LanguageConstants.languageFlag]);
-      return false;
-    }
-    if (questionAnswer.length < 1) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.dialogRegisterUnSuccessQuestionAnswerMessage[
-          LanguageConstants.languageFlag]);
-      return false;
-    }
-    if (selectedQuestion == null) {
-      Dialogs.showAlertMessage(
-          context,
-          LanguageConstants
-              .dialogUnSuccessHeader[LanguageConstants.languageFlag],
-          LanguageConstants.enterLanguageFlag[LanguageConstants.languageFlag]);
-      return false;
-    }
-
-    return true;
   }
 
   Future<void> createCustomer(
