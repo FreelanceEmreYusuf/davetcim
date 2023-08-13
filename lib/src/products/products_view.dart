57b851183bb7203dd9bd343ed0660c2209817103
emreatalay <emreatalay79@gmail.com>
2023-08-13 14:02:08 +0300

    20230813 product view page - future builder removed, circular indicator added

--- lib/src/products/products_view.dart
+++ lib/src/products/products_view.dart
@@ -22,6 +22,13 @@ class ProductsScreen extends StatefulWidget {
 class _ProductsScreenState extends State<ProductsScreen>  {
 
   List<CorporationModel> corporationList = [];
+  bool hasDataTaken = false;
+
+  @override
+  void initState() {
+    getCorporationList();
+    super.initState();
+  }
 
   Future getCorporationList() async {
     if (widget.corporationInputList != null) {
@@ -30,52 +37,60 @@ class _ProductsScreenState extends State<ProductsScreen>  {
       ProductsViewModel mdl = new ProductsViewModel();
       corporationList = await mdl.getCorporationList(widget.filter);
     }
+
+    setState(() {
+      corporationList = corporationList;
+      hasDataTaken = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: (context, projectSnap) {
-        return Scaffold(
-          appBar: AppBarMenu(pageName: "Salonlar", isHomnePageIconVisible: true, isNotificationsIconVisible: true, isPopUpMenuActive: true),
+    if (!hasDataTaken) {
+      return Scaffold(appBar:
+      AppBarMenu(pageName: "Salonlar", isHomnePageIconVisible: true, isNotificationsIconVisible: true, isPopUpMenuActive: true),
           body: Padding(
-            padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-            child: ListView(
-              children: <Widget>[
-                GridView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: MediaQuery.of(context).size.width /
-                        (MediaQuery.of(context).size.height / 1.15),
-                  ),
-                  itemCount: corporationList == null
-                      ? 0
-                      : corporationList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    CorporationModel item = corporationList[index];
-                    return GridProduct(
-                      img: item.imageUrl,
-                      isFav: ApplicationSession.isCorporationFavorite(item.corporationId),
-                      name: item.corporationName,
-                      rating: item.averageRating,
-                      raters: item.ratingCount,
-                      description: item.description,
-                      corporationId: item.corporationId,
-                      maxPopulation: item.maxPopulation,
-                      callerPage: ProductsScreen(widget.filter, corporationList),
-                    );
-                  },
+              padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+              child: Center(child: CircularProgressIndicator())));
+    }
+
+      return Scaffold(
+        appBar: AppBarMenu(pageName: "Salonlar", isHomnePageIconVisible: true, isNotificationsIconVisible: true, isPopUpMenuActive: true),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+          child: ListView(
+            children: <Widget>[
+              GridView.builder(
+                shrinkWrap: true,
+                primary: false,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: MediaQuery.of(context).size.width /
+                      (MediaQuery.of(context).size.height / 1.15),
                 ),
-                SizedBox(height: 20.0),
-              ],
-            ),
+                itemCount: corporationList == null
+                    ? 0
+                    : corporationList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  CorporationModel item = corporationList[index];
+                  return GridProduct(
+                    img: item.imageUrl,
+                    isFav: ApplicationSession.isCorporationFavorite(item.corporationId),
+                    name: item.corporationName,
+                    rating: item.averageRating,
+                    raters: item.ratingCount,
+                    description: item.description,
+                    corporationId: item.corporationId,
+                    maxPopulation: item.maxPopulation,
+                    callerPage: ProductsScreen(widget.filter, corporationList),
+                  );
+                },
+              ),
+              SizedBox(height: 20.0),
+            ],
           ),
-        );
-      },
-      future: getCorporationList(),
-    );
+        ),
+      );
   }
 }
