e0ab0f4825e0d3f49d0f5775dcfc724994ab800e
emreatalay <emreatalay79@gmail.com>
2023-09-20 19:40:20 +0300

    20230920 - test

--- lib/shared/maps/menu_back_map.dart
+++ lib/shared/maps/menu_back_map.dart
@@ -1,11 +1,11 @@
+import 'package:davetcim/src/admin_panel/AdminPanel.dart';
 import 'package:flutter/cupertino.dart';
-import '../../src/main/main_screen_view.dart';
 
 Map<String, Widget> menuBackMap =
 {
   'Davetçim': null,
   'Admin Paneli': null,
- // 'Fotoğraf Ekle': MainScreen(),
+  'Firma Aktif/Pasif Yönet': AdminPanelPage(),
 };
 
 
