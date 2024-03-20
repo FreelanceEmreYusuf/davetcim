76f75dcde7bdb5f625e5b54e2b3a8c2c5dba0b91
yusufenesdanalioglu <yusufenes3494@gmail.com>
2024-03-20 17:26:07 +0300

    ok

--- lib/src/admin_corporate_panel/manage_corporation_photos/pick_page.dart
+++ lib/src/admin_corporate_panel/manage_corporation_photos/pick_page.dart
@@ -1,92 +1,96 @@
 import 'package:davetcim/widgets/app_bar/app_bar_view.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../../../shared/utils/utils.dart';
 import '../manage_corporation_photos_add/manage_corporation_photos_add_view.dart';
 import 'manage_corporation_photos_view.dart';
 
-
-class PickPageView extends StatefulWidget {
+class PickPageView extends StatelessWidget {
   @override
-  _PickPageViewState createState() => _PickPageViewState();
-}
-
-class _PickPageViewState extends State<PickPageView> {
-  @override
-  Widget build(BuildContext contex){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarMenu(isPopUpMenuActive: true, isNotificationsIconVisible: true, isHomnePageIconVisible: true, pageName: "Fotoğraf Yönetimi"),
-      body:Center(
+      appBar: AppBarMenu(
+        isPopUpMenuActive: true,
+        isNotificationsIconVisible: true,
+        isHomnePageIconVisible: true,
+        pageName: "Fotoğraf Yönetimi",
+      ),
+      body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
           child: Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40)),
-                color: Colors.redAccent),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Card(
-                      elevation: 30,
-                      shadowColor: Colors.redAccent,
-                      child: Container(
-                        child: MaterialButton(
-                          onPressed: (){
-                              Utils.navigateToPage(context, ManageCorporationPhotosView());
-                          },
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.redAccent,
-                              child: Icon(
-                                Icons.photo,
-                                color: Colors.white,
-                                size: 30.0,
-                              ),
-                            ),
-                            title: Text("Resimleri Düzenle", style: TextStyle(color: Colors.redAccent, fontSize: 30.0, fontWeight: FontWeight.bold)),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Card(
-                      elevation: 30,
-                      shadowColor: Colors.redAccent,
-                      child: Container(
-                        child: MaterialButton(
-                          onPressed: (){
-                            Utils.navigateToPage(context, ManageCorporationPhotosAddView());
-                          },
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.redAccent,
-                              child: Icon(
-                                Icons.photo_camera,
-                                color: Colors.white,
-                                size: 30.0,
-                              ),
-                            ),
-                            title: Text("Resim Ekle", style: TextStyle(color: Colors.redAccent, fontSize: 30.0, fontWeight: FontWeight.bold)),
-
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
               ),
+              color: Colors.redAccent,
             ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: buildCard(
+                    context,
+                    "Resimleri Düzenle",
+                    Icons.photo,
+                    ManageCorporationPhotosView(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  flex: 1,
+                  child: buildCard(
+                    context,
+                    "Resim Ekle",
+                    Icons.photo_camera,
+                    ManageCorporationPhotosAddView(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCard(
+      BuildContext context, String title, IconData icon, Widget destination) {
+    return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        onTap: () {
+          Utils.navigateToPage(context, destination);
+        },
+        borderRadius: BorderRadius.circular(20),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.redAccent,
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+              ),
+              SizedBox(width: 20),
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ),
       ),
